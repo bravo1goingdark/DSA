68bd6daf556b2ae67eff0f83639910a3ef5a485a
bravo1goingdark <kumarashutosh34169@gmail.com>
2023-06-21 23:58:02 +0530

    valid anagram

---
+++ String/question.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class question {
+    public boolean isAnagram(String s, String t) {
+        // Check if the lengths of the strings are different
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        // Convert the strings to character arrays
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+        // Sort the character arrays
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        
+        // Compare the sorted arrays
+        for (int i = 0; i < sChars.length; i++) {
+            if (sChars[i] != tChars[i]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+}
