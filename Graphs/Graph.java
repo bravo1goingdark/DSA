c053d3abd07789953a269dad861199b01c6a8460
bravo1goingdark <kumarashutosh34169@gmail.com>
2023-10-22 23:59:51 +0530

    bi-directional graph implementation

--- Graphs/Graph.java
+++ Graphs/Graph.java
@@ -1,82 +1,33 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
-import java.util.Stack;
-
-public class Graph{
+public class Graph {
     private int numVertices;
     private List<List<Integer>> adjacencyList;
 
-    public Graph(int numVertices) {
+
+    Graph(int numVertices){
         this.numVertices = numVertices;
         this.adjacencyList = new ArrayList<>(numVertices);
         for (int i = 0; i < numVertices; i++) {
             adjacencyList.add(new ArrayList<>());
         }
     }
-    // bi-directional graph
-    public void addEdges(int source, int destination) {
+
+    public void addEdges(int source , int destination) {
         adjacencyList.get(source).add(destination);
         adjacencyList.get(destination).add(source);
     }
 
-    public void removeEdges(int source, int destination) {
-        List<Integer> sourceList = adjacencyList.get(source);
-        List<Integer> destList = adjacencyList.get(destination);
-
-        if (sourceList != null) {
-            sourceList.remove(Integer.valueOf(destination));
-        }
-        if (destList != null) {
-            destList.remove(Integer.valueOf(source));
-        }
+    public void removeEdges(int source , int destination) {
+        adjacencyList.get(source).remove(Integer.valueOf(destination));
+        adjacencyList.get(destination).remove(Integer.valueOf(source));
     }
-    public List<Integer> getadjacentVertices(int vertex) {
+
+    public List<Integer> getAdjacentVertices(int vertex) {
         return adjacencyList.get(vertex);
     }
-    public void bfs(int startVertex){
-        boolean[] visited = new boolean[numVertices];
-        Queue<Integer> que = new LinkedList<>();
-        visited[startVertex] = true;
-        que.offer(startVertex);
 
-        while (!que.isEmpty()) {
-            int currVertex = que.poll();
-            System.out.print(currVertex + " ");
-
-            List<Integer> list = adjacencyList.get(currVertex);
-            for (int adjacent : list) {
-                if (!visited[adjacent]) {
-                    visited[adjacent] = true;
-                    que.offer(adjacent);
-                }
-            }
-        }
-        System.out.println();
-    }
-    public void dfs(int startVertex){
-        boolean[] visited = new boolean[numVertices];
-        Stack<Integer> stack = new Stack<>();
-        visited[startVertex] = true;
-        stack.push(startVertex);
-
-        while (!stack.isEmpty()) {
-            int currVertex = stack.pop();
-            System.out.print(currVertex + " ");
-
-            List<Integer> neighbour = adjacencyList.get(currVertex);
-            for (int adj : neighbour) {
-                if (!visited[adj]) {
-                    visited[adj] = true;
-                    stack.push(adj);
-                }
-            }
-
-        }
-    }
-
-    public void display() {
+    public void printGraph() {
         for (int i = 0; i < numVertices; i++) {
             List<Integer> neighbors = adjacencyList.get(i);
             System.out.print(i + " -> ");
@@ -86,4 +37,5 @@ public class Graph{
             System.out.println();
         }
     }
+
 }
--- Graphs/Main.java
+++ Graphs/Main.java
@@ -1,45 +1,13 @@
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        Graph graph = new Graph(9);
-        graph.addEdges(1, 4);
-        graph.addEdges(1, 2);
-        graph.addEdges(4, 3);
-        graph.addEdges(2, 3);
-        graph.addEdges(2, 5);
-        graph.addEdges(2, 8);
-        graph.addEdges(5, 8);
-        graph.addEdges(5, 7);
-        graph.addEdges(7, 8);
-        graph.addEdges(5, 6);
-        graph.addEdges(2, 7);
-        graph.display();
+        Graph graph = new Graph(4);
+        graph.addEdges(0,1);
+        graph.addEdges(0,2);
+        graph.addEdges(2,1);
+        graph.addEdges(1,3);
+        graph.printGraph();
         System.out.println();
-
-        graph.bfs(1);
-        System.out.println();
-        graph.dfs(1);
-        System.out.println();
-        System.out.println();
-        
-        graph.removeEdges(1, 4);
-        graph.display();
-        System.out.println();
-        List<Integer> list = graph.getadjacentVertices(1);
-        System.out.println(list);
-        System.out.println();
-
-
-        PrimsAlgorithm graph2 = new PrimsAlgorithm(11);
-        graph2.addEdges(10, 1, 5);
-        graph2.addEdges(10, 2, 1);
-        graph2.addEdges(1, 2, 2);
-        graph2.addEdges(1, 3, 3);
-        graph2.addEdges(2, 3, 4);
-        graph2.addEdges(2, 4, 6);
-        graph2.addEdges(3, 4, 7);
-
-        graph2.primsMST(10);
+        graph.removeEdges(0, 1);
+        graph.printGraph();
     }
 }
