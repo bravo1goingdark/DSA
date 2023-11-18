02a6d9b7a362f9b4a68a32b1fb48da6b5968d2da
bravo1goingdark <kumarashutosh34169@gmail.com>
2023-11-18 16:21:39 +0530

    update and query method in segment tree

--- Trees/Question.java
+++ Trees/Question.java
@@ -5,7 +5,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-import javax.swing.tree.TreeNode;
 
 public class Question {
     public static void main(String[] args) {
--- Trees/SegmentTree.java
+++ Trees/SegmentTree.java
@@ -1,4 +1,10 @@
 public class SegmentTree {
+    public static void main(String[] args) {
+        SegmentTree tree = new SegmentTree(new int[]{3,8,7,6,-2,-8,4,9});
+        System.out.println(tree.query(2, 6));
+        tree.update(2, 100);
+        System.out.println(tree.query(2, 5));
+    }
     private static class Node {
         int value;
         int startInterval;
@@ -32,5 +38,41 @@ public class SegmentTree {
 
         newNode.value = newNode.left.value + newNode.right.value;
         return newNode;
+    }
+
+    public int query(int qsi, int qei) {
+        return this.query(this.root, qsi, qei);
+    }
+
+    private int query(Node node, int qsi, int qei) {
+        if(node.startInterval >= qsi && node.endInterval <= qei) {
+          // node is completely lying inside query
+          return node.value;
+        } else if (node.startInterval > qei || node.endInterval < qsi) {
+          // completely outside
+          return 0;
+        } else {
+          return this.query(node.left, qsi, qei) + this.query(node.right, qsi, qei);
+        }
+      }
+
+    public void update (int index , int value){
+        this.root.value = update(this.root , index, value);
+    }
+
+    private int update(Node node, int index, int value) {
+        if (index >= node.startInterval && index <= node.endInterval) {
+            if (index == node.startInterval && index == node.endInterval) {
+                node.value = value;
+                return node.value;
+            } else {
+                int leftAns = update(node.left, index, value);
+                int rightAns = update(node.right, index, value);
+                node.value = leftAns + rightAns;
+                return node.value;
+            }
+        }
+
+        return node.value;
     }
 }
