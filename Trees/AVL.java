b1207524ad9eb661f838c8c7a13e1717e1b712fe
bravo1goingdark <kumarashutosh34169@gmail.com>
2023-11-24 01:35:41 +0530

    implemented AVL tree

--- Trees/AVL.java
+++ Trees/AVL.java
@@ -1,112 +1,169 @@
-class AVLTree {
-    Node root;
-    
-    int height(Node node) {
-        if (node == null)
-        return 0;
-        return node.height;
+class AVL{
+    public static void main(String[] args) {
+        AVL tree = new AVL();
+        for (int i = 0; i < 1000; i++) {
+            tree.insert(i);
+        }
+        System.out.println(tree.height()); // without avl it will return 1000 but using avl it returns 3 due to height being log(N)
     }
 
-    int max(int a, int b) {
-        return (a > b) ? a : b;
-    }
-    
-    Node rotateRight(Node node) {
-        Node parent = node.left;
-        Node child = parent.right;
-        
-        parent.right = node;
-        node.left = child;
-        
-        node.height = max(height(node.left), height(node.right)) + 1;
-        parent.height = max(height(parent.left), height(parent.right)) + 1;
-        
-        return parent;
-    }
-    
-    Node rotateLeft(Node parent) {
-        Node node = parent.right;
-        Node child = node.left;
-        
-        node.left = parent;
-        parent.right = child;
-        
-        parent.height = max(height(parent.left), height(parent.right)) + 1;
-        node.height = max(height(node.left), height(node.right)) + 1;
-        
-        return node;
-    }
-    
-    int balanceFactor(Node node) {
-        if (node == null) {
-            return 0;
-        }
-        return height(node.left) - height(node.right);
-    }
-    
-    Node insert(Node node, int value) {
-        if (node == null)
-        return (new Node(value));
-        
-        if (value < node.value)
-        node.left = insert(node.left, value);
-        else if (value > node.value)
-        node.right = insert(node.right, value);
-        else
-        return node;
-        
-        node.height = 1 + max(height(node.left), height(node.right));
-        
-        int balance = balanceFactor(node);
-        
-        if (balance > 1 && value < node.left.value)
-        return rotateRight(node);
+  public class Node {
+    private int value;
+    private Node left;
+    private Node right;
+    private int height;
 
-        if (balance < -1 && value > node.right.value)
-            return rotateLeft(node);
-            
-            if (balance > 1 && value > node.left.value) {
-                node.left = rotateLeft(node.left);
-                return rotateRight(node);
-            }
-            
-            if (balance < -1 && value < node.right.value) {
-                node.right = rotateRight(node.right);
-                return rotateLeft(node);
-            }
-            
-            return node;
-        }
-        
-        void preOrder(Node node) {
-            if (node != null) {
-                System.out.print(node.value + " ");
-                preOrder(node.left);
-                preOrder(node.right);
-            }
-        }
-        
-        public static void main(String[] args) {
-            AVLTree tree = new AVLTree();
+    public Node(int value) {
+      this.value = value;
+    }
 
-            tree.root = tree.insert(tree.root, 10);
-            tree.root = tree.insert(tree.root, 20);
-            tree.root = tree.insert(tree.root, 30);
-            tree.root = tree.insert(tree.root, 40);
-            tree.root = tree.insert(tree.root, 50);
-            tree.root = tree.insert(tree.root, 25);
-            
-            System.out.println("Preorder traversal of constructed tree is:");
-            tree.preOrder(tree.root);
-        }
+    public int getValue() {
+      return value;
     }
-    
-    class Node {
-        int value, height;
-        Node left, right;
-    
-        Node(int d) {
-            value = d;
-            height = 1;
-        }
+  }
+
+  private Node root;
+
+  public AVL() {
+
+  }
+
+  public int height() {
+    return height(root);
+  }
+  private int height(Node node) {
+    if (node == null) {
+      return -1;
     }
+    return node.height;
+  }
+  
+  public void insert(int value) {
+    root = insert(value, root);
+  }
+
+  private Node insert(int value, Node node) {
+    if (node == null) {
+      node = new Node(value);
+      return node;
+    }
+
+    if (value < node.value) {
+      node.left = insert(value, node.left);
+    }
+
+    if (value > node.value) {
+      node.right = insert(value, node.right);
+    }
+
+    node.height = Math.max(height(node.left), height(node.right)) + 1;
+    return rotate(node);
+  }
+
+  private Node rotate(Node node) {
+    if (height(node.left) - height(node.right) > 1) {
+      // left heavy
+      if(height(node.left.left) - height(node.left.right) > 0) {
+        // left left case
+        return rightRotate(node);
+      }
+      if(height(node.left.left) - height(node.left.right) < 0) {
+        // left right case
+        node.left = leftRotate(node.left);
+        return rightRotate(node);
+      }
+    }
+
+    if (height(node.left) - height(node.right) < -1) {
+      // right heavy
+      if(height(node.right.left) - height(node.right.right) < 0) {
+        // right right case
+        return leftRotate(node);
+      }
+      if(height(node.right.left) - height(node.right.right) > 0) {
+        // left right case
+        node.right = rightRotate(node.right);
+        return leftRotate(node);
+      }
+    }
+
+    return node;
+  }
+
+  public Node rightRotate(Node p) {
+    Node c = p.left;
+    Node t = c.right;
+
+    c.right = p;
+    p.left = t;
+    
+    p.height = Math.max(height(p.left), height(p.right) + 1);
+    c.height = Math.max(height(c.left), height(c.right) + 1);
+
+    return c;
+  }
+
+  public Node leftRotate(Node c) {
+    Node p = c.right;
+    Node t = p.left;
+
+    p.left = c;
+    c.right = t;
+    
+    p.height = Math.max(height(p.left), height(p.right) + 1);
+    c.height = Math.max(height(c.left), height(c.right) + 1);
+
+    return p;
+  }
+
+  public void populate(int[] nums) {
+    for (int i = 0; i < nums.length; i++) {
+      this.insert(nums[i]);
+    }
+  }
+
+  public void populatedSorted(int[] nums) {
+    populatedSorted(nums, 0, nums.length);
+  }
+
+  private void populatedSorted(int[] nums, int start, int end) {
+    if (start >= end) {
+      return;
+    }
+
+    int mid = (start + end) / 2;
+
+    this.insert(nums[mid]);
+    populatedSorted(nums, start, mid);
+    populatedSorted(nums, mid + 1, end);
+  }
+
+  public void display() {
+    display(this.root, "Root Node: ");
+  }
+
+  private void display(Node node, String details) {
+    if (node == null) {
+      return;
+    }
+    System.out.println(details + node.value);
+    display(node.left, "Left child of " + node.value + " : ");
+    display(node.right, "Right child of " + node.value + " : ");
+  }
+
+  public boolean isEmpty() {
+    return root == null;
+  }
+
+  public boolean balanced() {
+    return balanced(root);
+  }
+
+  private boolean balanced(Node node) {
+    if (node == null) {
+      return true;
+    }
+    return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
+  }
+
+}
--- Trees/Red_Black.java
+++
@@ -1,3 +0,0 @@
-public class Red_Black {
-    
-}
